e9145a35aaacf933ec80eada827107c3939bdafc
Samip <callmesamip@gmail.com>
2022-09-03 20:44:04 +1000

    Loader is done

--- src/components/loader-comps/LoaderComponent.js
+++ src/components/loader-comps/LoaderComponent.js
@@ -3,12 +3,7 @@ import React from 'react';
 export const LoaderComponent = () => {
 	return (
 		<div className="d-flex flex-column justify-content-center align-items-center">
-			<p className="text-lead fs-4">Hey!</p>
-			<p className="text-muted">
-				Santa tells me, you haven't clicked on a name
-			</p>
-			<p className="text-muted">Please click on a name to get started</p>
-			<p className="text-lead">and the magic happens...</p>
+			<p className="text-light lead">Loading.....</p>
 		</div>
 	);
 };
