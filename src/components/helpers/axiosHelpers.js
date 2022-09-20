74a27e3c3f3819173d4ad00f405a4be9bb4ae4de
Samip <callmesamip@gmail.com>
2022-09-20 23:32:00 +1000

    Added a comment

--- src/components/helpers/axiosHelpers.js
+++ src/components/helpers/axiosHelpers.js
@@ -3,7 +3,7 @@ const APIEndPoint = 'https://jsonplaceholder.typicode.com/users';
 
 export const fetchUsersFromApi = async () => {
 	try {
-		const result = await axios.get(APIEndPoint);
+		const result = await axios.get(APIEndPoint); //get data from API endPoint
 		return result.data;
 	} catch (error) {
 		return;
